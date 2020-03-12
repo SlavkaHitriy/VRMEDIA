ff3f78e0effddb971d45793d134f664500802b84
Slavka <slavik.nukutuk@gmail.com>
2020-03-12 23:58:42 +0200

    mobile full

--- app/js/main.js
+++ app/js/main.js
@@ -13,10 +13,9 @@ $(window).on('load', function () {
 $(function () {
 
    $(window).on('scroll', function () {
-      
-      if ($(window).scrollTop() > 800) {
+
+      if ($(window).scrollTop() > $('.franchise__first-title').offset().top) {
          $('.franchise__logo').removeClass('show')
-         // console.log('1')
          let slide = 0;
          for (let i = $('.franchise__slider > .franchise__slider-item').last().index(); i > 1; i--) {
             if ($('.franchise__slider-item')[i].offsetTop >= $(window).scrollTop()) {
@@ -25,28 +24,66 @@ $(function () {
          }
          $('.franchise__slider-item').each(function () {
             if ($(this).attr('id').match(/\d{1,2}/)[0] == slide - 1) {
-               if($('.franchise__text-flash').text() == $(this).find('.franchise-title-global').text()){
-                  $('.franchise__text-flash').addClass('show') 
+               if ($(this).find('.franchise-title-global').text().trim() == '') {
+                  for (let item of $(this).prevAll()) {
+                     if (item.querySelector('.franchise-title-global') == null) { } else {
+                        $('.franchise__text-flash').text(item.querySelector('.franchise-title-global').textContent);
+                        return false;
+                     }
+                  }
+                  $('.franchise__text-flash').text($(this).prev().find('.franchise-title-global').text());
+                  return false;
+               }
+               if ($('.franchise__text-flash').text() == $(this).find('.franchise-title-global').text()) {
+                  $('.franchise__text-flash').addClass('show')
                } else {
                   $('.franchise__text-flash').text($(this).find('.franchise-title-global').text());
                   $('.franchise__text-flash').removeClass('show')
                }
             }
          })
-      } else{
+      } else {
          $('.franchise__logo').addClass('show')
          $('.franchise__text-flash').removeClass('show')
       }
    });
 
-   $('.franchise__burger').on('click', function(){
+   $('.franchise__burger').on('click', function () {
       $('.franchise__burger-menu').addClass('show')
       $('.franchise__burger-menu').removeClass('hide')
+      let addDelay = 1.1;
+
+      $('.franchise__burger-menu-inner li').each(function () {
+         addDelay -= 0.1;
+         $(this).find('a').css('transition-delay', addDelay + 's');
+      })
    })
-   $('.franchise__burger-close').on('click', function(){
+   $('.franchise__burger-close').on('click', function () {
       $('.franchise__burger-menu').removeClass('show')
       $('.franchise__burger-menu').addClass('hide')
+
+      $('.franchise__burger-menu-inner li').each(function () {
+         $(this).find('a').css('transition-delay', '0s');
+      })
    })
+
+   $(".franchise__burger-menu-inner").on("click", "a", function (e) {
+      e.preventDefault();
+
+      $('.franchise__burger-menu').removeClass('show')
+      $('.franchise__burger-menu').addClass('hide')
+
+      $('.franchise__burger-menu-inner li').each(function () {
+         $(this).find('a').css('transition-delay', '0s');
+      })
+
+      var id = $(this).attr('href'),
+         top = $(id).offset().top;
+
+      $('body,html').animate({ scrollTop: top - 50 }, 500);
+
+   });
+
 
    enquire.register("screen and (min-width:1000px)", {
       match: function () {
@@ -819,11 +856,17 @@ $(function () {
       }
    }));
 
-   $('.franchise__burger-menu').on('click', function (e) {
+   $('.franchise__burger-link').on('click', function (e) {
       e.preventDefault();
+      $('.franchise__burger-menu-inner li').each(function () {
+         $(this).find('a').css('transition-delay', '0s');
+      })
+      $('.franchise__burger-menu').addClass('hide');
+      $('.franchise__burger-menu').removeClass('show');
       let top = $('#slide10').offset().top;
       $('body,html').animate({ scrollTop: top }, 500);
    })
+
 
    enquire.register("screen and (max-width:1000px)", {
 
